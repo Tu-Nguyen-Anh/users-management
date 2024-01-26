5c96c158eb6245f65eccc7aa3de4920b569fa987
nguyenanhtu123 <lovebooks29062k2@gmail.com>
2024-01-26 14:00:54 +0700

    update

--- demo/src/main/java/com/example/demo/exception/base/ConflictException.java
+++ demo/src/main/java/com/example/demo/exception/base/ConflictException.java
@@ -5,7 +5,7 @@ import static com.example.demo.constant.Message.ExceptionStatusConstant.CONFLICT
 
 public class ConflictException extends BaseException {
   public ConflictException(String objectName) {
-    setCode("com.ncsgroup.login.exception.base.ConflictException");
+    setCode("com.example.demo.exception.base.ConflictException");
     setStatus(CONFLICT);
     addParam("objectName", objectName);
   }
